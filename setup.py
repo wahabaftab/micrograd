e1352eb8e2b158059815cd8d46c134e82760fff9
Wahab <s.wahabaftab@gmail.com>
2025-03-23 23:45:04 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A tiny scalar-valued autograd engine with a small PyTorch-like neural network library on top.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/karpathy/micrograd",
+    url="https://github.com/wahabaftab/micrograd",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
